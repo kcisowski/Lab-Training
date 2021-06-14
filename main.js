6c171816b74b3779d02c760ba04f0a37df7421ac
kc <83770699+kcisowski@users.noreply.github.com>
2021-06-14 12:49:00 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+
+
+
+  function formatDate(date) {
+    var cdate = new Date(date);
+    var options = {
+      year: "numeric",
+      month: "short",
+      day: "numeric"
+    };
+    date = cdate.toLocaleDateString("en-us", options);
+    return date;
+  }
+
+
+ 
+
+  
+
+
+// $(function () {
+//   var client = ZAFClient.init();
+//   client.invoke("resize", { width: "100%", height: "200px" });
+//   client.get('ticket.requester.id').then(
+//     function(data) {
+//       var user_id = data['ticket.requester.id'];
+//       //var user_id = 10293389;
+//       console.log('Requester id is ' + user_id);
+//       requestUserInfo(client, user_id);
+//     }
+//   );
+//   //showInfo();
+//   //showError();
+  
+// });
+
+
